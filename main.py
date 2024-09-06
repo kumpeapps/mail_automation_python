659c600388a57041592ac4a521070ca5ee4ddd06
Justin Kumpe <jakumpe@kumpes.com>
2024-09-06 17:55:44 -0500

    -

--- main.py
+++ main.py
@@ -16,23 +16,24 @@ for line in sys.stdin:
     full_msg += line
 
 msg = email.message_from_string(full_msg)
+logger.debug(msg)
 
-to = msg['to']
-from_email = msg['from']
-subject = msg['subject']
+# to = msg['to']
+# from_email = msg['from']
+# subject = msg['subject']
 
-#make an emty variable for email body
-body = ""
+# #make an emty variable for email body
+# body = ""
 
-#if the message contains attaachments find the body attachment
-#if not find the entire emial body
-if msg.is_multipart():
-    for payload in msg.get_payload():
-    # if payload.is_multipart(): …
-        body = payload.get_payload()
-else:
-    body = msg.get_payload()
+# #if the message contains attaachments find the body attachment
+# #if not find the entire emial body
+# if msg.is_multipart():
+#     for payload in msg.get_payload():
+#     # if payload.is_multipart(): …
+#         body = payload.get_payload()
+# else:
+#     body = msg.get_payload()
 
 
 
-logger.debug(f"To: {to}\nFrom: {from_email}\nSubject: {subject}\nBody:\n{body}")
+# logger.debug(f"To: {to}\nFrom: {from_email}\nSubject: {subject}\nBody:\n{body}")
